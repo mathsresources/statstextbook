faa387b4fa1686b8258b557021248df362bfdb2d
mathsresources <andyjames.1875@icloud.com>
2026-05-14 07:45:01 +0100

    ch18

--- randdata.R
+++ randdata.R
@@ -1,13 +1,30 @@
 library(truncnorm)
 
-x<-runif(10,0.1,4.9)
-e<-rtruncnorm(10,-1.9,1.9,0,0.1)
-a<--1
-b<-0.7
-y<-a+0.5*(x-3)^2+e
-resid <- data.frame(x,y)
+x<-c(1:10)
+e<-0.1*ceiling(rtruncnorm(10,-50,50,0,25))
+a<-1
+b<-0.1
+y <- x^2+e
+df_stopping <- data.frame(x,y)
 plot(x,y)
 
+y <- sqrty^2
+df_transf_before <- data.frame(x,y)
+
+df_transf_before_residuals <- data.frame(y,resid(lm(y~x)))
+
+df_transf_after_residuals <- data.frame(sqrty,resid(lm(sqrty~x)))
+#plot(y,resid(lm(y~x)))
+
+par(mfrow=(c(1,1)))
+plot(x,y)
+plot(y,resid(lm(y~x)))
+plot(x,sqrty)
+plot(y,resid(lm(sqrty~x)))
+
+sqrty <- y^0.5
+
+plot(x,sqrty)
 
 sums <- function(dfr){
   attach(dfr)
