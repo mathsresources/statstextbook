f0a13eaae689861956fa29dd95e3e0e3d5b08333
Andy James <andyjames1875@outlook.com>
2026-05-14 17:02:04 +0100

    ch18

--- randdata.R
+++ randdata.R
@@ -16,11 +16,21 @@ df_transf_before_residuals <- data.frame(y,resid(lm(y~x)))
 df_transf_after_residuals <- data.frame(sqrty,resid(lm(sqrty~x)))
 #plot(y,resid(lm(y~x)))
 
-par(mfrow=(c(1,1)))
+
+x<-c(16.7,21.0,26.8,29.4,34.7,42.3,51.7,58.0,65.5,73.2)
+y<-c(6,8,13,18,22,38,51,66,82,107)
+sqrty<-y^0.5
+par(mfrow=(c(2,2)))
 plot(x,y)
 plot(y,resid(lm(y~x)))
 plot(x,sqrty)
-plot(y,resid(lm(sqrty~x)))
+plot(sqrty,resid(lm(sqrty~x)))
+df_stoppingdata <- data.frame(x,y)
+df_stoppingresid <- data.frame(y,residuals(lm(y~x)))
+df_stoppingdatat <- data.frame(x,sqrty)
+df_stoppingresidt <- data.frame(sqrty,residuals(lm(sqrty~x)))
+
+summary(lm(sqrty~x))
 
 sqrty <- y^0.5
 
@@ -29,7 +39,7 @@ plot(x,sqrty)
 sums <- function(dfr){
   attach(dfr)
   sxx <- sum(x^2) - ((sum(x))^2)/length(x)
-  syy <- sum(y) - ((sum(y))^2)/length(y)
+  syy <- sum(y^2) - ((sum(y))^2)/length(y)
   sxy <- sum(x*y) - (sum(x)*sum(y))/length(x)
   print(sxx)
   print(syy)
@@ -37,6 +47,10 @@ sums <- function(dfr){
   detach(dfr)
 }
 
-sums(df_bivariatetestq1)
+rm(x)
+rm(y)
+
+
+sums(df_stoppingdatat)
 
 
